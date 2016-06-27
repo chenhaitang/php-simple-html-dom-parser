ca3c5c82ef0befbdd0aac40a011c72bad2fb3710
chenhaitang <912998466@qq.com>
2016-06-27 20:03:14 +0800

    update file

--- Src/Trident/PhpSimple/htmlDomPaeser.php
+++ Src/Trident/PhpSimple/htmlDomPaeser.php
@@ -1,21 +1,21 @@
 <?php
-namespace chenhaitang;
+namespace Trident\htmlDom;
 
 require 'simple_html_dom.php';
 
-class HtmlDom {
+class HtmlDomParser {
 	
 	/**
 	 * @return \simplehtmldom_1_5\simple_html_dom
 	 */
 	static public function file_get_html() {
-		return call_user_func_array ( '\file_get_html' , func_get_args() );
+		return call_user_func_array ( '\simplehtmldom_1_0\file_get_html' , func_get_args() );
 	}
 	/**
 	 * get html dom from string
 	 * @return \simplehtmldom_1_5\simple_html_dom
 	 */
 	static public function str_get_html() {
-		return call_user_func_array ( '\str_get_html' , func_get_args() );
+		return call_user_func_array ( '\simplehtmldom_1_0\str_get_html' , func_get_args() );
 	}
 }
